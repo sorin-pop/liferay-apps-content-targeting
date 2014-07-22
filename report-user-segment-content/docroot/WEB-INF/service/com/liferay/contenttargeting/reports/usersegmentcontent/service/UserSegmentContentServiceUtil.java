5ef0e9373d5ea0fa29fd6c3e8a07fc4ef758f961
Julio Camarero <julio.camarero@liferay.com>
2014-07-22 12:48:14 +0200

    WCM-97 move user segment report content files

---
+++ report-user-segment-content/docroot/WEB-INF/service/com/liferay/contenttargeting/reports/usersegmentcontent/service/UserSegmentContentServiceUtil.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.reports.usersegmentcontent.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+
+/**
+ * Provides the remote service utility for UserSegmentContent. This utility wraps
+ * {@link com.liferay.contenttargeting.reports.usersegmentcontent.service.impl.UserSegmentContentServiceImpl} and is the
+ * primary access point for service operations in application layer code running
+ * on a remote server. Methods of this service are expected to have security
+ * checks based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author Brian Wing Shun Chan
+ * @see UserSegmentContentService
+ * @see com.liferay.contenttargeting.reports.usersegmentcontent.service.base.UserSegmentContentServiceBaseImpl
+ * @see com.liferay.contenttargeting.reports.usersegmentcontent.service.impl.UserSegmentContentServiceImpl
+ * @generated
+ */
+@ProviderType
+public class UserSegmentContentServiceUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to {@link com.liferay.contenttargeting.reports.usersegmentcontent.service.impl.UserSegmentContentServiceImpl} and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	public static java.lang.String getBeanIdentifier() {
+		return getService().getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	public static void setBeanIdentifier(java.lang.String beanIdentifier) {
+		getService().setBeanIdentifier(beanIdentifier);
+	}
+
+	public static UserSegmentContentService getService() {
+		if (_service == null) {
+			_service = (UserSegmentContentService)PortalBeanLocatorUtil.locate(UserSegmentContentService.class.getName());
+
+			ReferenceRegistry.registerReference(UserSegmentContentServiceUtil.class,
+				"_service");
+		}
+
+		return _service;
+	}
+
+	/**
+	 * @deprecated As of 6.2.0
+	 */
+	public void setService(UserSegmentContentService service) {
+	}
+
+	private static UserSegmentContentService _service;
+}
