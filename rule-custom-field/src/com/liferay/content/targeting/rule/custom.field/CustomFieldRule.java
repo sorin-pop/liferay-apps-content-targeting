16d1442bd706c2bbe48a9050ac05efb37848dc0e
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-01-26 16:13:09 +0100

    WCM-212 Implement Custom Field Rule

--- rule-custom-field/src/com/liferay/content/targeting/rule/custom.field/CustomFieldRule.java
+++ rule-custom-field/src/com/liferay/content/targeting/rule/custom.field/CustomFieldRule.java
@@ -14,14 +14,34 @@
 
 package com.liferay.content.targeting.rule.custom.field;
 
+import com.liferay.content.targeting.InvalidRuleException;
 import com.liferay.content.targeting.anonymous.users.model.AnonymousUser;
 import com.liferay.content.targeting.api.model.BaseRule;
 import com.liferay.content.targeting.api.model.Rule;
 import com.liferay.content.targeting.model.RuleInstance;
-import com.liferay.content.targeting.rule.categories.SampleRuleCategory;
-import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.content.targeting.rule.categories.UserAttributesRuleCategory;
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.permission.ResourceActionsUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.expando.model.ExpandoBridge;
+import com.liferay.portlet.expando.service.ExpandoValueLocalServiceUtil;
+import com.liferay.portlet.expando.util.ExpandoBridgeFactoryUtil;
 
+import java.io.Serializable;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -35,7 +55,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 
 /**
- * @author Brian Chan
+ * @author Eudaldo Alonso
  */
 @Component(immediate = true, service = Rule.class)
 public class CustomFieldRule extends BaseRule {
@@ -53,12 +73,61 @@ public class CustomFieldRule extends BaseRule {
 	}
 
 	@Override
+	public void deleteData(RuleInstance ruleInstance)
+		throws PortalException, SystemException {
+
+		JSONObject jsonObj = JSONFactoryUtil.createJSONObject(
+			ruleInstance.getTypeSettings());
+
+		long classPK = jsonObj.getLong("classPK");
+
+		ExpandoValueLocalServiceUtil.deleteValues(
+			User.class.getName(), classPK);
+	}
+
+	@Override
 	public boolean evaluate(
 			HttpServletRequest request, RuleInstance ruleInstance,
 			AnonymousUser anonymousUser)
 		throws Exception {
 
+		if (anonymousUser.getUserId() != 0) {
+			ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+			JSONObject jsonObj = JSONFactoryUtil.createJSONObject(
+				ruleInstance.getTypeSettings());
+
+			String attributeName = jsonObj.getString("attributeName");
+			long classPK = jsonObj.getLong("classPK");
+
+			ExpandoBridge expandoBridge =
+				ExpandoBridgeFactoryUtil.getExpandoBridge(
+					themeDisplay.getCompanyId(), User.class.getName(), classPK);
+
+			Serializable attribute = expandoBridge.getAttribute(attributeName);
+
+			ExpandoBridge userExpandoBridge =
+				ExpandoBridgeFactoryUtil.getExpandoBridge(
+					themeDisplay.getCompanyId(), User.class.getName(),
+					anonymousUser.getUserId());
+
+			Serializable userAttribute = userExpandoBridge.getAttribute(
+				attributeName);
+
+			if (attribute.equals(userAttribute)) {
+				return true;
+			}
+		}
+
 		return false;
+
+		/*
+		ExpandoValue expandoValue = ExpandoValueLocalServiceUtil.getValue(
+			companyId, className, tableName, columnName, classPK);
+
+		expandoBridge.getAttribute()
+		*/
 	}
 
 	@Override
@@ -68,26 +137,100 @@ public class CustomFieldRule extends BaseRule {
 
 	@Override
 	public String getRuleCategoryKey() {
-		return SampleRuleCategory.KEY;
+		return UserAttributesRuleCategory.KEY;
 	}
 
 	@Override
 	public String getSummary(RuleInstance ruleInstance, Locale locale) {
-		return LanguageUtil.get(locale, ruleInstance.getTypeSettings());
+		return StringPool.BLANK;
 	}
 
 	@Override
 	public String processRule(
-		PortletRequest request, PortletResponse response, String id,
-		Map<String, String> values) {
+			PortletRequest request, PortletResponse response, String id,
+			Map<String, String> values)
+		throws InvalidRuleException {
 
-		return StringPool.BLANK;
+		long classPK = GetterUtil.getLong(values.get("classPK"));
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		try {
+			if (classPK <= 0) {
+				classPK = CounterLocalServiceUtil.increment();
+			}
+
+			Map<String, Serializable> expandoBridgeAttributes =
+				PortalUtil.getExpandoBridgeAttributes(
+					ExpandoBridgeFactoryUtil.getExpandoBridge(
+						themeDisplay.getCompanyId(), User.class.getName()),
+					request);
+
+			ExpandoBridge expandoBridge =
+				ExpandoBridgeFactoryUtil.getExpandoBridge(
+					themeDisplay.getCompanyId(), User.class.getName(), classPK);
+
+			expandoBridge.setAttributes(expandoBridgeAttributes);
+		}
+		catch (Exception e) {
+			throw new InvalidRuleException("cannot-save-custom-fields");
+		}
+
+		String attributeName = values.get("attributeName");
+
+		JSONObject jsonObj = JSONFactoryUtil.createJSONObject();
+
+		jsonObj.put("attributeName", attributeName);
+		jsonObj.put("classPK", classPK);
+
+		return jsonObj.toString();
 	}
 
 	@Override
 	protected void populateContext(
 		RuleInstance ruleInstance, Map<String, Object> context,
 		Map<String, String> values) {
+
+		context.put("className", User.class.getName());
+
+		Locale locale = (Locale)context.get("locale");
+
+		context.put(
+			"modelResourceName",
+			ResourceActionsUtil.getModelResource(locale, User.class.getName()));
+
+		String attributeName = StringPool.BLANK;
+		long classPK = 0;
+
+		if (!values.isEmpty()) {
+			classPK = GetterUtil.getLong(values.get("classPK"));
+		}
+		else if (ruleInstance != null) {
+			try {
+				JSONObject jsonObj = JSONFactoryUtil.createJSONObject(
+					ruleInstance.getTypeSettings());
+
+				attributeName = jsonObj.getString("attributeName");
+				classPK = jsonObj.getLong("classPK");
+			}
+			catch (JSONException jse) {
+			}
+		}
+
+		context.put("attributeName", attributeName);
+		context.put("classPK", classPK);
+
+		Company company = (Company)context.get("company");
+
+		ExpandoBridge expandoBridge =
+			ExpandoBridgeFactoryUtil.getExpandoBridge(
+				company.getCompanyId(), User.class.getName(), classPK);
+
+		List<String> attributeNames = Collections.list(
+			expandoBridge.getAttributeNames());
+
+		context.put("attributeNames", attributeNames);
 	}
 
 }
